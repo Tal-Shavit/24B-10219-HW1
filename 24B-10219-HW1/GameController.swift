8acce25377accccd534d7d0b8aeff77b308fb28b
Tal-Shavit <talshavit84@gmail.com>
2024-07-05 15:43:28 +0300

    add timer and back card

--- 24B-10219-HW1/GameController.swift
+++ 24B-10219-HW1/GameController.swift
@@ -3,51 +3,62 @@ import Foundation
 import UIKit
 
 protocol CallBack_Score {
-    func score(score1: Int, score2: Int, card1Value: Int?, card2Value: Int?)
+    func score(currentCard: Int, card1Value: Int?, card2Value: Int?, showBack: Bool)
 }
 
 class Detector {
     var callback: CallBack_Score?
     var timer: Timer?
+    var currentCard = 0
+    var timerCount = 0
     var score1 = 0
     var score2 = 0
-    var cardsCounter = 0
     let cardValues: [String: Int]
     let imagesPlayer1: [UIImage]
     let imagesPlayer2: [UIImage]
+    let backCardImage: UIImage?
     
-    init(callBack: CallBack_Score, cardValues: [String: Int], imagesPlayer1: [UIImage], imagesPlayer2: [UIImage]) {
+    init(callBack: CallBack_Score, cardValues: [String: Int], imagesPlayer1: [UIImage], imagesPlayer2: [UIImage], backCardImage: UIImage?) {
         self.callback = callBack
         self.cardValues = cardValues
         self.imagesPlayer1 = imagesPlayer1
         self.imagesPlayer2 = imagesPlayer2
+        self.backCardImage = backCardImage
     }
     
     func startGame() {
-        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: change(t:))
-    }
-    
-    func change(t: Timer) {
-        if cardsCounter < imagesPlayer1.count {
-            let card1 = imagesPlayer1[cardsCounter]
-            let card2 = imagesPlayer2[cardsCounter]
-            
-            let card1Value = cardValues[getImageName(for: card1)] ?? 0
-            let card2Value = cardValues[getImageName(for: card2)] ?? 0
-            
-            if card1Value > card2Value {
-                score1 += 1
-            } else if card2Value > card1Value {
-                score2 += 1
-            }
-            
-            callback?.score(score1: score1, score2: score2, card1Value: card1Value, card2Value: card2Value)
-            
-            cardsCounter += 1
-        } else {
-            callback?.score(score1: score1, score2: score2, card1Value: nil, card2Value: nil)
-            t.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: updateGame(t:))
         }
+    
+    func updateGame(t: Timer){
+        switch timerCount{
+        case 0:
+            if currentCard < imagesPlayer1.count {
+                let card1 = imagesPlayer1[currentCard]
+                let card2 = imagesPlayer2[currentCard]
+                
+                let card1Value = cardValues[getImageName(for: card1)] ?? 0
+                let card2Value = cardValues[getImageName(for: card2)] ?? 0
+               
+                callback?.score(currentCard: currentCard, card1Value: card1Value, card2Value: card2Value, showBack: false)
+                
+                if card1Value > card2Value{
+                    score1 += 1
+                }
+                else if card2Value > card1Value{
+                    score2 += 1
+                }
+
+            } else {
+                t.invalidate()
+            }
+        case 3:
+            callback?.score(currentCard: currentCard, card1Value: nil, card2Value: nil, showBack: true)
+            currentCard += 1
+        default:
+            break
+        }
+        timerCount = (timerCount + 1) % 5
     }
     
     func getImageName(for image: UIImage) -> String {
--- 24B-10219-HW1/ViewController.swift
+++ 24B-10219-HW1/ViewController.swift
@@ -1,27 +1,18 @@
-//
-//  ViewController.swift
-//  24B-10219-HW1
-//
-//  Created by Student8 on 01/07/2024.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var main_IMG_card1: UIImageView!
     @IBOutlet weak var main_IMG_card2: UIImageView!
-    @IBOutlet weak var main_LBL_number1: UILabel!
-    @IBOutlet weak var main_LBL_number2: UILabel!
     @IBOutlet weak var main_LBL_score1: UILabel!
     @IBOutlet weak var main_LBL_score2: UILabel!
     
-    var imagesPlayer1 = [#imageLiteral(resourceName: "8_of_spades"), #imageLiteral(resourceName: "2_of_diamonds"), #imageLiteral(resourceName: "9_of_hearts"), #imageLiteral(resourceName: "1_of_clubs"), #imageLiteral(resourceName: "3_of_diamonds"), #imageLiteral(resourceName: "2_of_diamonds"), #imageLiteral(resourceName: "10_of_clubs"), #imageLiteral(resourceName: "7_of_spades"), #imageLiteral(resourceName: "3_of_diamonds"), #imageLiteral(resourceName: "1_of_spades")]
+    var imagesPlayer1 = [#imageLiteral(resourceName: "4_of_hearts"), #imageLiteral(resourceName: "2_of_diamonds"), #imageLiteral(resourceName: "9_of_hearts"), #imageLiteral(resourceName: "1_of_clubs"), #imageLiteral(resourceName: "3_of_diamonds"), #imageLiteral(resourceName: "2_of_diamonds"), #imageLiteral(resourceName: "10_of_clubs"), #imageLiteral(resourceName: "7_of_spades"), #imageLiteral(resourceName: "3_of_diamonds"), #imageLiteral(resourceName: "1_of_spades")]
     var imagesPlayer2 = [#imageLiteral(resourceName: "3_of_diamonds"), #imageLiteral(resourceName: "11_of_hearts"), #imageLiteral(resourceName: "4_of_clubs"), #imageLiteral(resourceName: "12_of_clubs"), #imageLiteral(resourceName: "2_of_clubs"), #imageLiteral(resourceName: "3_of_diamonds"), #imageLiteral(resourceName: "12_of_clubs"), #imageLiteral(resourceName: "9_of_spades"), #imageLiteral(resourceName: "1_of_hearts"), #imageLiteral(resourceName: "1_of_diamonds")]
-    var currentIndex = 0
-    var score1 = 0
-    var score2 = 0
-    var myDetector: Detector?
+    var score1 = 0, score2 = 0
+    var myDetector : Detector?
+    let backCardImage = UIImage(named: "card")
     
     
     let cardValues: [String: Int] = [
@@ -35,6 +26,7 @@ class ViewController: UIViewController {
         "1_of_spades": 1,
         "11_of_hearts": 11,
         "4_of_clubs": 4,
+        "4_of_hearts": 4,
         "12_of_clubs": 12,
         "2_of_clubs": 2,
         "9_of_spades": 9,
@@ -43,41 +35,45 @@ class ViewController: UIViewController {
     ]
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        myDetector = Detector(callBack: self, cardValues: cardValues, imagesPlayer1: imagesPlayer1, imagesPlayer2: imagesPlayer2)
-        myDetector?.startGame()
-    }
-            
-    func updateUI(card1Value: Int?, card2Value: Int?) {
-        if currentIndex < imagesPlayer1.count {
-            let card1 = imagesPlayer1[currentIndex]
-            let card2 = imagesPlayer2[currentIndex]
-                    
-            main_IMG_card1.image = card1
-            main_IMG_card2.image = card2
-                    
-            main_LBL_number1.text = "\(card1Value)"
-            main_LBL_number2.text = "\(card2Value)"
-            main_LBL_score1.text = "\(score1)"
-            main_LBL_score2.text = "\(score2)"
-                    
-            currentIndex += 1
-        }
-        }
-    }
+           super.viewDidLoad()
+           main_LBL_score1.text = "\(0)"
+           main_LBL_score2.text = "\(0)"
+        main_IMG_card1.image = backCardImage
+        main_IMG_card2.image = backCardImage
+           myDetector = Detector(callBack: self, cardValues: cardValues, imagesPlayer1: imagesPlayer1, imagesPlayer2: imagesPlayer2, backCardImage: backCardImage)
+           myDetector?.startGame()
+       }
+   }
 
-    extension ViewController: CallBack_Score {
-        func score(score1: Int, score2: Int, card1Value: Int?, card2Value: Int?) {
-            self.score1 = score1
-            self.score2 = score2
-            if currentIndex >= imagesPlayer1.count {
-                main_LBL_number1.text = "Game Over"
-                main_LBL_number2.text = ""
-                main_IMG_card1.image = nil
-                main_IMG_card2.image = nil
-            }else {
-                updateUI(card1Value: card1Value, card2Value: card2Value)
-            }
-            
-        }
-    }
+   extension ViewController: CallBack_Score {
+       func score(currentCard: Int, card1Value: Int?, card2Value: Int?, showBack: Bool) {
+           if showBack {
+               main_IMG_card1.image = backCardImage
+               main_IMG_card2.image = backCardImage
+           } else if currentCard < imagesPlayer1.count {
+               let card1 = imagesPlayer1[currentCard]
+               let card2 = imagesPlayer2[currentCard]
+               
+              changeScore(card1Value: card1Value, card2Value: card2Value)
+               
+               main_IMG_card1.image = card1
+               main_IMG_card2.image = card2
+           }
+       }
+       
+       func changeScore(card1Value: Int?, card2Value: Int?){
+           if let card1Value = card1Value, let card2Value = card2Value {
+               DispatchQueue.main.asyncAfter(deadline: .now()+1){
+                   if(card1Value > card2Value){
+                       self.score1 += 1
+                       self.main_LBL_score1.text = "\(self.score1)"
+                   }
+                   else if(card2Value > card1Value){
+                       self.score2 += 1
+                       self.main_LBL_score2.text = "\(self.score2)"
+                   }
+               }
+               
+           }
+       }
+   }
